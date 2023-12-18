f17071aac894c57d5570373a7f6f4f0204863faf
V-Niclos <109752029+V-Niclos@users.noreply.github.com>
2023-12-18 10:48:19 +0100

    Add files via upload

---
+++ belen_alexa_8_reles_2023_12_14/documentation/Readme.txt
@@ -0,0 +1,6 @@
+Modulo esp32 con 8 reles integrado
+Power 5v 2a
+Conexion a Alexa
+Alexa names of relays [1...8] Belen 1... Belen 8
+Conexion ip fija 192.168.2.254
+Pins relay const int g_pinRelays[g_RelaysCount] = {13,12,14,27,26,25,33,32};
---
+++ belen_alexa_8_reles_2023_12_14/src/mainDefines.h
@@ -0,0 +1,48 @@
+#ifndef MAINDEFINES_H
+#define MAINDEFINES_H
+#include <Arduino.h>
+#include "AsyncJson.h"
+#include "ArduinoJson.h"
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include "fauxmoESP.h"
+#include "ClsRelay.h"
+#include "ClsTimeSun.h"
+#include "ClsWifiConfig.h"
+#define SERIAL_BAUDRATE  115200
+  StaticJsonDocument<1024> g_docJsonRelays;
+ClsTimeSun g_TimeSun;
+ClsWifiConfig g_Config;
+//------ RELAYS
+uint32_t g_RelayDelayLoop = 1;
+const int g_RelaysCount = 8;
+ClsRelay g_Relays[g_RelaysCount];
+//------ HADWARE PINS USED
+const int g_pinIntLedBlue =2;
+const int g_pingSensorLed = 23;
+//int g_pinRelays[4] = {26, 25, 33, 32}; 
+//int g_pinRelays[8] = {23, 21, 19, 4, 25, 16, 13, 26};
+//int g_pinRelays[g_RelaysCount] =   {16, 14, 12, 13, 15, 2,4,5 }; 
+//int g_pinRelays[g_RelaysCount] =   {13, 12, 14, 13, 15, 2,4,5 }; 
+const int g_pinRelays[g_RelaysCount] = {13,12,14,27,26,25,33,32};
+//16, 14, 12, 13, 15, 0,4, 5
+//16, 14, 12, 13, 15, 2,4,5,
+
+//-------------- NET SERVICES 
+unsigned long g_AlexaLastMillis =0;
+fauxmoESP g_NetFauxmoAlexa;
+AsyncWebServer g_NetWebServer(80);
+// THIS IS THE NAMES joined to reralys TO SAY alexa command 
+// example like: "Alexa! turn on relay 1"
+const char g_AlexaDeviceNames[8][8]={{"belen 1"},{"belen 2"},{"belen 3"},{"belen 4"},{"belen 5"},{"belen 6"},{"belen 7"},{"belen 8"}};
+
+TaskHandle_t g_MainTaskAlexaHandle;
+TaskHandle_t g_MainTaskTimeSunHandle;
+TaskHandle_t g_MainTaskRelaysHandle;
+void g_MainTaskAlexa(void *pvParameters);
+void g_MainTaskTimeSun(void *pvParameters);
+void g_MainTaskRelays(void *pvParameters);
+
+bool fncRelaysJsonApply(String sJson);
+
+#endif
